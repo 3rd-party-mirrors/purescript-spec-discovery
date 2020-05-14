fd27873c72bf92f3a35b971d7abbb5578be496cd
rafael-brandao <rafa.bra@gmail.com>
2020-05-14 14:37:03 -0300

    Reformat files using purty and prettier.

--- src/Test/Spec/Discovery.js
+++ src/Test/Spec/Discovery.js
@@ -1,20 +1,27 @@
-'use strict';
+"use strict";
 
-if (typeof require !== 'function') {
-  throw new Error('Sorry, purescript-spec-discovery only supports NodeJS environments!');
+if (typeof require !== "function") {
+  throw new Error(
+    "Sorry, purescript-spec-discovery only supports NodeJS environments!"
+  );
 }
 
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 function getMatchingModules(pattern) {
-  var directories = fs.readdirSync(path.join(__dirname, '..'));
-  return directories.filter(function (directory) {
-    return (new RegExp(pattern).test(directory));
-  }).map(function (name) {
-    var module = require(path.join(__dirname, '..', name));
-    return (module && typeof module.spec !== 'undefined') ? module.spec : null;
-  }).filter(function (x) { return x; });
+  var directories = fs.readdirSync(path.join(__dirname, ".."));
+  return directories
+    .filter(function (directory) {
+      return new RegExp(pattern).test(directory);
+    })
+    .map(function (name) {
+      var module = require(path.join(__dirname, "..", name));
+      return module && typeof module.spec !== "undefined" ? module.spec : null;
+    })
+    .filter(function (x) {
+      return x;
+    });
 }
 
 exports.getSpecs = function (pattern) {
--- src/Test/Spec/Discovery.purs
+++ src/Test/Spec/Discovery.purs
@@ -1,15 +1,18 @@
 module Test.Spec.Discovery (discover) where
 
 import Prelude
-
 import Data.Traversable (sequence_)
 import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
 import Test.Spec (Spec)
 
-foreign import getSpecs :: String
-                        -> Effect (Array (Spec Unit))
+foreign import getSpecs ::
+  String ->
+  Effect (Array (Spec Unit))
 
-discover :: forall m. MonadEffect m => String
-         -> m (Spec Unit)
+discover ::
+  forall m.
+  MonadEffect m =>
+  String ->
+  m (Spec Unit)
 discover pattern = getSpecs pattern >>= (pure <<< sequence_) # liftEffect
